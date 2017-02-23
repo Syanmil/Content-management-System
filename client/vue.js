d952f080f46cc9194c90d63ee62059dfb2d4870a
Syanmil <syanmil@yahoo.com>
2017-02-23 13:40:40 +0700

    add login logout and menu bar

--- client/vue.js
+++ client/vue.js
@@ -3,10 +3,16 @@ var app = new Vue({
   data: {
     message: 'Hello World',
     authenticated: false,
+    page: 'landing',
     data: [],
     dataDate: []
   },
   methods: {
+    login: function(){
 
+    },
+    logout: function(){
+      
+    }
   }
 })
