40eea305a63b537774fc90537937aed10404d1b3
Syanmil <syanmil@yahoo.com>
2017-02-23 14:09:01 +0700

    login register

--- client/vue.js
+++ client/vue.js
@@ -3,16 +3,39 @@ var app = new Vue({
   data: {
     message: 'Hello World',
     authenticated: false,
-    page: 'landing',
+    page: "landing",
+    username: '',
+    password: '',
     data: [],
     dataDate: []
   },
   methods: {
     login: function(){
-
+      app.page = 'login'
     },
     logout: function(){
-      
+      app.page = 'landing'
+    },
+    register: function(){
+      app.page = 'register'
+    },
+    signin: function(){
+      axios.post('http://localhost:3000/api/users/login', {
+        username: app.username,
+        password: app.password
+      })
+      .then(function(data){
+        sessionStorage.setItem('token', data.token)
+      })
+    },
+    signup: function(){
+      axios.post('http://localhost:3000/api/users/login', {
+        username: app.username,
+        password: app.password
+      })
+      .then(function(data) {
+        
+      })
     }
   }
 })
